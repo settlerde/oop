388f25acc6bd113c76a2834257660140a1e93311
settlerde <260442816+settlerde@users.noreply.github.com>
2026-04-27 10:37:01 +0200

    oop3

--- oop03.dart
+++ oop03.dart
@@ -1,39 +1,42 @@
-enum Planet { Earth, Mars, Jupiter, Moon }
+enum Planet { earth, mars, jupiter, moon }
 
 class Weight {
-  double kg;
+  final double kg;
 
   Weight._internal(this.kg);
 
-  // Weight.fromMars(double weight) : this._internal(weight * 0.38);
-  // Weight.fromMoon(double weight) : this._internal(weight * 0.16);
-  // Weight.fromJupiter(double weight) : this._internal(weight * 2.36);
+  Weight.onMars(double weight) : this._internal(weight * 0.38);
+  Weight.onMoon(double weight) : this._internal(weight * 0.16);
+  Weight.onJupiter(double weight) : this._internal(weight * 2.36);
 
   Weight.fromPlanet(double weight, Planet planet)
     : this._internal(_convertWeight(weight, planet));
 
   static double _convertWeight(double weight, Planet planet) {
     switch (planet) {
-      case Planet.Earth:
+      case Planet.earth:
         return weight;
-      case Planet.Mars:
+      case Planet.mars:
         return weight * 0.38;
-      case Planet.Jupiter:
+      case Planet.jupiter:
         return weight * 2.36;
-      case Planet.Moon:
+      case Planet.moon:
         return weight * 0.16;
     }
   }
 }
 
 void main() {
-  var myWWeightOnEarth = Weight.fromPlanet(93, Planet.Earth);
-  var myWeightOnMars = Weight.fromPlanet(93, Planet.Mars);
-  var myWeightOnJupiter = Weight.fromPlanet(93, Planet.Jupiter);
-  var myWeightOnMoon = Weight.fromPlanet(93, Planet.Moon);
+  var myWWeightOnEarth = Weight.fromPlanet(93, Planet.earth);
+  var myWeightOnMars = Weight.fromPlanet(93, Planet.mars);
+  var myWeightOnJupiter = Weight.fromPlanet(93, Planet.jupiter);
+  var myWeightOnMoon = Weight.fromPlanet(93, Planet.moon);
 
   print("Gewicht auf der Erde: ${myWWeightOnEarth.kg} kg");
   print("Gewicht auf dem Mars: ${myWeightOnMars.kg} kg");
   print("Gewicht auf dem Jupiter: ${myWeightOnJupiter.kg} kg");
   print("Gewicht auf dem Mond: ${myWeightOnMoon.kg} kg");
+
+  print(myWeightOnMoon.kg);
+  print(Weight.onMars(93).kg);
 }
