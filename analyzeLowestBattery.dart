f8b65f7311abe85f7e3fb815fcf4615c2e6d1cae
settlerde <settleaipost@gmail.com>
2026-04-30 21:51:45 +0200

    DeviceService

---
+++ analyzeLowestBattery.dart
@@ -0,0 +1,67 @@
+class DeviceService {
+  static List<int> getBatteryLevels() {
+    return [45, 22, 28, 10, 15, 99];
+  }
+}
+
+void analyzeLowestBattery() {
+  List<int> batteryLevels = DeviceService.getBatteryLevels();
+
+  int minAkku = batteryLevels[0];
+  int minIndex = 0;
+  int criticalCount = 0;
+
+  for (int i = 0; i < batteryLevels.length; i++) {
+    if (batteryLevels[i] < minAkku) {
+      minAkku = batteryLevels[i];
+      minIndex = i + 1;
+    }
+    if (batteryLevels[i] < 20) {
+      criticalCount++;
+    }
+  }
+  print('Niedrigster Akkustand: $minAkku %');
+  print('Gerät-Index: $minIndex');
+  print('Kritische Geräte (<20%): $criticalCount');
+}
+
+void main() {
+  analyzeLowestBattery();
+}
+
+/*
+START
+
+CLASS DeviceService
+    METHOD getBatteryLevels(): INTEGER[]
+        RETURN [45, 12, 78, 19, 5]
+    END METHOD
+END CLASS
+
+FUNCTION analyzeLowestBattery()
+
+    SET batteryLevels = DeviceService.getBatteryLevels()
+
+    SET min = batteryLevels[0]
+    SET minIndex = 0
+    SET criticalCount = 0
+
+    FOR i FROM 0 TO length(batteryLevels)
+
+        IF batteryLevels[i] < min THEN
+            SET min = batteryLevels[i]
+            SET minIndex = i
+        END IF
+
+        IF batteryLevels[i] < 20 THEN
+            SET criticalCount = criticalCount + 1
+        END IF
+
+    END FOR
+
+    OUTPUT "Niedrigster Akkustand: " + min + "% (Gerät-Index: " + minIndex + "), Kritische Geräte (<20%): " + criticalCount
+
+END FUNCTION
+
+END
+*/
