d6aaa5d32e6656ce1f2a83da2b408e6f66653e7c
settlerde <260442816+settlerde@users.noreply.github.com>
2026-05-07 14:20:09 +0200

    oop4 Teilnehmer

--- flutter_oop/lib/oop04.dart
+++ flutter_oop/lib/oop04.dart
@@ -1,40 +1,57 @@
 import 'dart:math';
 
-// Komposition
+// --- Komposition (Composition) ---
+
+/// Repräsentiert die geschlechtliche Identität eines Teilnehmers.
+/// Wird als [enum] verwendet, um Typischerheit zu gewärleisten.
 enum Geschlecht { maennlich, weiblich, divers }
 
+/// Generiert einen einduetiger Code für jeden Teilnehmer.
 class Zutrittsberechtigung {
+  /// Ein zufällig generierter numerischer Code.
   final int code;
 
+  /// Erstellt eine Instanz mit einem Zufallswert zwischen 0 und 999.999.999.
   Zutrittsberechtigung() : code = Random().nextInt(1000000000);
 }
 
+/// Repräsentiert eine Person, die an einem Kurs teilnehmt.
+/// Enthält persönliche Daten und eine [Zutrittsberechtigung].
 class Teilnehmer {
   final String vorname;
   final String name;
   final Geschlecht geschlecht;
 
+  /// Die Berechtigung wird direkt bei der Erstellung des Teilnehmers generiert.
   final Zutrittsberechtigung berechtigung;
 
+  /// Ausgibt die [Zutrittsberechtigung].
   Teilnehmer(this.vorname, this.name, this.geschlecht)
     : berechtigung = Zutrittsberechtigung();
 }
 
-// Aggregation
+// --- Aggregation ---
+
+/// Repräsentiert einen Bildungskurs mit einem Titel und einer Liste von Teilnemern.
 class Kurs {
   final String titel;
   final List<Teilnehmer> teilnehmerListe = [];
 
+  /// Konstruktor enthält nur Titel.
   Kurs(this.titel);
 
+  /// Die Methode nimmt einen Teilnehmer entgegen und fügt ihn zue Liste hinzu.
   void addTeilnehmer(Teilnehmer type) {
     teilnehmerListe.add(type);
   }
 }
 
+/// Die Hauptklasse der Akademie, die verschidene Kurse verwaltet.
 class Cdemy {
+  /// Eine Sammlung aller verfügbaren Kurse in der Akademie.
   final List<Kurs> kurse = [];
 
+  /// Die Methode nimmt einen Kurs entgegen und fügt ihn zu Liste hinzu.
   void addKurs(Kurs udemyKurs) {
     kurse.add(udemyKurs);
   }
@@ -44,28 +61,28 @@ void main() {
   Cdemy myCdemy = Cdemy();
 
   Kurs dartKurs = Kurs('Dart Profi');
-  Kurs flutterKurs = Kurs('Flutter zero to hero');
+  Kurs pythonKurs = Kurs('Python zero to hero');
   Kurs webDevKurs = Kurs('Web Development');
 
   myCdemy.addKurs(dartKurs);
-  myCdemy.addKurs(flutterKurs);
+  myCdemy.addKurs(pythonKurs);
   myCdemy.addKurs(webDevKurs);
-
+  // Erstellung der Teilnehmer
   Teilnehmer t1 = Teilnehmer('Olek', 'Melnychenko', Geschlecht.maennlich);
   Teilnehmer t2 = Teilnehmer('Maria', 'Jason', Geschlecht.weiblich);
   Teilnehmer t3 = Teilnehmer('Alex', 'Seasharp', Geschlecht.divers);
-
+  // Zuweisung der Teilnehmer zu den Kursen
   dartKurs.addTeilnehmer(t1);
   webDevKurs.addTeilnehmer(t2);
-  flutterKurs.addTeilnehmer(t3);
-
+  pythonKurs.addTeilnehmer(t3);
+  // Ausgabe der Struktur
   print('Cdemy Kurse:');
   for (var kurs in myCdemy.kurse) {
     print('-- ${kurs.titel} --');
     print('  Teilnehmer:');
     for (var teilnehmer in kurs.teilnehmerListe) {
       print(
-        '  - ${teilnehmer.vorname} ${teilnehmer.name} (${teilnehmer.geschlecht})',
+        '  - ${teilnehmer.vorname}, ${teilnehmer.name}, ${teilnehmer.geschlecht.name}, Code: ${teilnehmer.berechtigung.code}',
       );
     }
   }
