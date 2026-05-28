d9823b88755f5592f609f0fcf05df7d7c39da983
settlerde <settleaipost@gmail.com>
2026-05-28 21:37:20 +0200

    dieser Variante

--- flutter_oop/lib/lieferwagen.dart
+++ flutter_oop/lib/lieferwagen.dart
@@ -8,23 +8,21 @@ void findBestVan(int requiredVolume) {
   ]; // VanService.getVanCapacities();
 
   int bestVan = -1;
-  int maxVolume = 5000;
+  int maxVolume = 50000;
 
   for (int i = 0; i < vanVolumes.length; i++) {
-    if (vanVolumes[i] >= requiredVolume) {
-      if (vanVolumes[i] < maxVolume) {
-        bestVan = i;
-        maxVolume = vanVolumes[i];
-      }
+    if (vanVolumes[i] >= requiredVolume && vanVolumes[i] < maxVolume) {
+      bestVan = i;
+      maxVolume = vanVolumes[i];
     }
   }
   if (bestVan == -1) {
     print('Kein Lieferwagen ist groß genug');
   } else {
-    print('"Bester Lieferwagen: $maxVolume L (Index: $bestVan)');
+    print('Bester Lieferwagen: $maxVolume L (Index: $bestVan)');
   }
 }
 
 void main() {
-  findBestVan(1000);
+  findBestVan(500);
 }
